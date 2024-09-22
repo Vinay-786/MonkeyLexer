fbeb0b3c3f512e64cfae9c37e1816346296e53ae
Vinay Kumar <itsvinay.in@gmail.com>
2024-09-23 01:49:29 +0530

    extended support for more tokens
    Adding support for ==, !, !=, -, /, *, <, > and boolean + control flow
    keywords. Note: Two character token uses additional helper function
    peekChar() to peek the next char and then assigns right tokens

--- token/token.go
+++ token/token.go
@@ -11,12 +11,25 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
+	//Identifiers + Literal
 	IDENT = "IDENT"
 	INT   = "INT"
 
-	ASSIGN = "="
-	PLUS   = "+"
+	//Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	//Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
 
@@ -25,6 +38,29 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
+	//Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var Keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := Keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
